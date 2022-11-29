761ae88dcde729bbfce0e5bd874d261c32e2b2fa
István Kiss <serenereeverflow@gmail.com>
2022-11-29 11:34:20 +0100

    Added Tailwind CSS.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,45 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
 
-export default function Home() {
+export async function getServerSideProps() {
+  try {
+    const users = await fetch(
+      "https://jsonplaceholder.typicode.com/users"
+    ).then((res) => res.json());
+
+    return {
+      props: { users },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      props: {
+        users: [],
+        err,
+      },
+    };
+  }
+}
+
+export default function Home(props) {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>NextJS ServerSideRendering Project</title>
       </Head>
-
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <header>
+          <h1>Fake User List</h1>
+        </header>
+        <section>
+          {props.users.map((u) => {
+            return (
+              <article key={u.id} className='bg-slate-600 h-5'>
+                <h3 className={`text-red-500`}>{u.name}</h3>
+              </article>
+            );
+          })}
+        </section>
       </main>
-
-      <Footer />
-    </div>
-  )
+    </>
+  );
 }
